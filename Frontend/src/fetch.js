a4804c4bdc246884005882041f8742d9100376dd
Guilherme-Cavalanti <guilhermecavalanti@gmail.com>
2024-03-25 20:21:02 -0300

    fix bug

--- Frontend/src/fetch.js
+++ Frontend/src/fetch.js
@@ -11,10 +11,83 @@ const InsertTask = async (task) => {
         return response
     } catch (error) {
         console.log("Error inserting task:", error)
-        return ({"fail":"Failed to connect to server",error})
+        return ({ "fail": "Failed to connect to server", error })
+    }
+}
+
+const GetTasks = async () => {
+    try {
+        const response = await api.get("/")
+        const { data } = response
+        return data
+    } catch (error) {
+        console.log("Error getting tasks:", error)
+        return ({ "fail": "Failed to connect to server", error })
+    }
+}
+
+const GetTaskbById = async (id) => {
+    try {
+        const response = await api.get(`/${id}`)
+        const { data } = response
+        return data
+    } catch (error) {
+        console.log("Error getting tasks:", error)
+        if (error.code == "ERR_NETWORK") {
+            return ({ "message": "Failed to connect to server", error })
+        }
+        if (error.code == "ERR_BAD_REQUEST") {
+            const { response } = error
+            return ({ ...response.data, error })
+        }
+    }
+}
+
+const UpdateTask = async (id, task) => {
+    try {
+        const response = await api.put(`/update?id=${id}`, {
+            "title": task.title,
+            "priority": task.priority,
+            "status": task.status,
+            "dueDate": task.dueDate,
+            "description": task.description
+        })
+        const { data } = response
+        return data
+    } catch (error) {
+        console.log("Error getting tasks:", error)
+        if (error.code == "ERR_NETWORK") {
+            return ({ "message": "Failed to connect to server", error })
+        }
+        if (error.code == "ERR_BAD_REQUEST") {
+            const { response } = error
+            return ({ ...response.data, error })
+        }
+    }
+}
+
+const DeleteTask = async (id) => {
+    try {
+        const response = await api.delete(`/delete?id=${id}`)
+        const {data} = response
+        return data
+    } catch (error) {
+        console.log("Error getting tasks:", error)
+        if (error.code == "ERR_NETWORK") {
+            return ({ "message": "Failed to connect to server", error })
+        }
+        if (error.code == "ERR_BAD_REQUEST") {
+            const { response } = error
+            return ({ ...response.data, error })
+        }
     }
 }
 
 export default {
-    InsertTask
+    InsertTask,
+    GetTasks,
+    GetTaskbById,
+    UpdateTask,
+    DeleteTask
 }
+
